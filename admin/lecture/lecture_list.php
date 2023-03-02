abc9fe0ad7f65f149ce6ff94906378aa75743ddd
CelineSunrise <119560572+CelineSunrise@users.noreply.github.com>
2023-03-03 01:48:59 +0900

    lecture_list.php 업로드

---
+++ admin/lecture/lecture_list.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/common.css">
+</head>
+<body>
+<div class="common-wrap">
+  <div class="gnb-body pd-54">
+    <div class="admin-profile">
+      <div class="profile-img-wrap">
+        <img src="img/admin-profile.png" alt="admin-img">
+      </div>
+      <h2>Manager</h2>
+      <div class="profile-menu-wrap">
+        <ul class="profile-menu d-flex justify-content-center">
+          <li><a href=""><i class="fa-brands fa-whmcs"></i></a></li>
+          <li><a href=""><i class="fa-regular fa-calendar-check"></i></a></li>
+          <li><a href=""><i class="fa-brands fa-weixin"></i></a></li>
+        </ul>
+      </div>
+    </div>
+    <ul class="gnb-menu">
+      <li class="main-menu d-flex align-items-center justify-content-center"><i class="fa-solid fa-wrench"></i>대시보드</li>
+      <li class="main-menu d-flex align-items-center justify-content-center"><i class="fas fa-user-friends"></i>회원 관리</li>
+      <li class="main-menu d-flex align-items-center justify-content-center"><i class="fa-solid fa-book"></i>강좌 관리</li>
+      <li class="main-menu d-flex align-items-center justify-content-center"><i class="fas fa-money-check-alt"></i>매출 관리</li>
+      <li class="main-menu d-flex align-items-center justify-content-center"><i class="fas fa-bullhorn"></i>이벤트 관리</li>
+      <li class="main-menu d-flex align-items-center justify-content-center"><i class="fas fa-th-list"></i>게시판 관리</li>
+    </ul>
+  </div>
+  <div class="content-body content-pd">
+    <!-- 로고 및 북마크 위치 시작 -->
+    <div class="content-top">
+      <h1 id="main-logo"><a href="/"><img src="../img/fastcode_logo.png" alt="Fastcode"><span>fastcode</span></a></h1>
+      <div class="bookmark">
+        <input type="checkbox" id="bookmark1"/>
+        <label for="bookmark1"><i class="fa-solid fa-bookmark"></i></label>
+      </div>
+    </div>
+
+
+
+  </div>
+</div>
+</body>
+</html>

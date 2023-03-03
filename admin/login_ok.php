c15711d3b1c73675553cdb60fbd6beda802153f5
redseon <119560806+redseon@users.noreply.github.com>
2023-03-03 17:07:35 +0900

    Update login_ok.php

---
+++ admin/login_ok.php
@@ -0,0 +1,30 @@
+<?php 
+    session_start();
+    $_SESSION['AUID'];
+    $userid = $_POST["userid"];
+    $passwd = $_POST["passwd"];
+    $passwd = hash('sha512',$passwd);
+
+    $sql = "SELECT * from admins where userid='{$userid}' and passwd='{$passwd}'";
+    $result = $mysqli -> query($sql);
+    $rs = $result ->fetch_object();
+
+    if($rs){
+        $sql = "UPDATE admins set last_login=now() where idx = '{$rs->idx}'";
+        $result = $mysqli -> query($sql);
+        $_SESSION['AUID'] = $rs->userid;
+        $_SESSION['AUNAME'] = $rs->username;
+        $_SESSION['ALEVEL'] = $rs->level;
+        echo "<script>
+            alert('관리자님 어서오세요');
+            location.href='/admin/dashboard/index.php';
+        </script>";
+        exit;
+    } else{
+        echo "<script>
+            alert('아이디 또는 암호가 일치하지 않습니다.');
+            history.back();
+        </script>";
+        exit;
+    }
+?>

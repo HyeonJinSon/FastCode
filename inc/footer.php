ed940711367af476f44a0f5ccc9c25cf2443d4aa
HyeonJinSon <119560821+HyeonJinSon@users.noreply.github.com>
2023-03-06 15:32:03 +0900

    php 공통요소 업로드 (헤더, 사이드 메뉴, 컨테이너 크기, 푸터)

---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+    </div>
+</div>
+<script src="menu.js"></script>
+  <script>
+    document.addEventListener("DOMContentLoaded", function () {
+      new SimpleBar(document.querySelector("[data-simplebar]"));
+    });
+  </script>
+</body>
+
+</html>

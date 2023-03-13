c017ac6e0efdab8a308438a76fb9f87040727ff3
HyeonJinSon <119560821+HyeonJinSon@users.noreply.github.com>
2023-03-14 01:15:52 +0900

    jquery 로드 위치 이동

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 <script src="menu.js"></script>
   <script>
     document.addEventListener("DOMContentLoaded", function () {

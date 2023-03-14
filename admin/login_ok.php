4e7e7a52cc6fc9d64a91189bd6573dd9edf89967
redseon <119560806+redseon@users.noreply.github.com>
2023-03-14 14:38:49 +0900

    login_ok 수정

--- admin/login_ok.php
+++ admin/login_ok.php
@@ -1,7 +1,11 @@
 <?php 
     session_start();
-    $_SESSION['AUID'];
     include $_SERVER['DOCUMENT_ROOT']."/inc/db.php";
+    
+    $_SESSION['AUID'];
+    $_SESSION['ADBOOK'] = 0;
+    $_SESSION['ADIDX'];
+    
     $userid = $_POST["userid"];
     $passwd = $_POST["passwd"];
     $passwd = hash('sha512',$passwd);
@@ -10,15 +14,25 @@
     $result = $mysqli -> query($sql);
     $rs = $result ->fetch_object();
 
+
     if($rs){
         $sql = "UPDATE admins SET last_login = now() WHERE idx = '{$rs->idx}'";
         $result = $mysqli -> query($sql);
         $_SESSION['AUID'] = $rs->userid;
         $_SESSION['AUNAME'] = $rs->username;
-        $_SESSION['ALEVEL'] = $rs->level;
+        $_SESSION['ADLEVEL'] = $rs->level;
+        $_SESSION['ADIDX'] = $rs->idx;
+        
+        if($rs->bookmark_code == null) {
+            $sql_null = "UPDATE admins SET bookmark_code = 0 WHERE idx='{$rs->idx}' ";
+            $result_null = $mysqli -> query($sql_null);
+        } else {
+            $_SESSION['ADBOOK'] = $rs->bookmark_code;
+        }
+
         echo "<script>
             alert('{$rs->username}님 어서오세요');
-            location.href='/admin/dashboard/index.php';
+            location.href='dashboard/index.php';
         </script>";
         exit;
     } else{

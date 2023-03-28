da4702613f34ded9ddbe85bd0aaac209fdf3b5a5
mi222ng <mi222ng@naver.com>
2023-03-28 16:30:19 +0900

    카트 수정중

---
+++ user/cart/cart_del.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    include $_SERVER['DOCUMENT_ROOT']."/inc/db.php";
+
+    $cid = $_POST['cid'];
+    $sql = "DELETE from cart WHERE cartid='".$cid."'"; 
+
+    $result = $mysqli -> query($sql) or die("query Error! =>".$mysqli->error);
+
+    if($result){
+        $returned_data = array('result' => true);
+    } else{
+        $returned_data = array('result' => false);
+    }
+
+    echo json_encode($returned_data);
+
+?>

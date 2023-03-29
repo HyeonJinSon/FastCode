068374f328d4d3a1025bcf6c545902c11f740745
mi222ng <mi222ng@naver.com>
2023-03-29 17:39:22 +0900

    카트 수정중

--- user/cart/cart_insert.php
+++ user/cart/cart_insert.php
@@ -1,36 +1,36 @@
 <?php session_start();
-include $_SERVER["DOCUMENT_ROOT"]."/inc/dbcon.php";
-ini_set( 'display_errors', '0' );
+    include $_SERVER["DOCUMENT_ROOT"]."/inc/dbcon.php";
+    ini_set( 'display_errors', '0' );
 
-$lecid = $_POST['lecid'];
-$ssid = session_id();
-$userid = $_SESSION['UID'];
+    $pid = $_POST['pid'];
+    $ssid = session_id();
+    $userid = $_SESSION['UID'];
 
-$query = "select cartid from cart where ssid='".$ssid."' and lecid='".$lecid."'";
-$result = $mysqli->query($query) or die("query error => ".$mysqli->error);
-$rs = $result->fetch_object();
-if($rs->cartid){
-    $sql="update cart set where ssid='".$ssid."' and lecid='".$lecid."'";
-    $result=$mysqli->query($sql) or die($mysqli->error);
-}else{
-    $sql="INSERT INTO `cart`
-    (`lecid`,
-    `userid`,
-    `ssid`,
-    `regdate`)
-    VALUES
-    ('".$lecid."',
-    '".$userid."',
-    '".$ssid."',
-    now())";
-    $result=$mysqli->query($sql) or die($mysqli->error);
-}
-if($result){
-    $data = array("result"=>"ok");
-}else{
-    $data = array("result"=>"fail");
-}
-echo json_encode($data);
+    $query = "select cartid from cart where ssid='".$ssid."' and pid='".$pid."'";
+    $result = $mysqli->query($query) or die("query error => ".$mysqli->error);
+    $rs = $result->fetch_object();
+    if($rs->cartid){
+        $sql="update cart set where ssid='".$ssid."' and pid='".$pid."'";
+        $result=$mysqli->query($sql) or die($mysqli->error);
+    }else{
+        $sql="INSERT INTO `cart`
+        (`pid`,
+        `userid`,
+        `ssid`,
+        `regdate`)
+        VALUES
+        ('".$pid."',
+        '".$userid."',
+        '".$ssid."',
+        now())";
+        $result=$mysqli->query($sql) or die($mysqli->error);
+    }
+    if($result){
+        $data = array("result"=>"ok");
+    }else{
+        $data = array("result"=>"fail");
+    }
+    echo json_encode($data);
 
 
 ?>

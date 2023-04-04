72f7497e01d482d72c93e028ec439360aa77d8cb
HyeonJinSon <119560821+HyeonJinSon@users.noreply.github.com>
2023-04-04 18:27:18 +0900

    Add files via upload

---
+++ inc/user/tag.php
@@ -0,0 +1,48 @@
+<?php
+    $post_time = $lr->reg_date;
+    $week = date("Y-m-d", strtotime($time_now."+7 days"));
+    if($post_time <= $week){
+        $icon_new = '<span class="mini-tag new-tag">new</span>';
+    } else{
+        $icon_new='';
+    }
+
+    $sale_cnt = $lr->sale_cnt;
+    if($sale_cnt > 100){
+        $icon_hot = '<span class="mini-tag hit-tag">hit</span>';
+    }else{
+        $icon_hot ='';
+    }
+    $recom = $lr->recom;
+    if($recom == 1){
+        $icon_recom = '<span class="mini-tag hit-tag">추천</span>';
+    }else{
+        $icon_recom ='';
+    }
+
+    $forbegin = $lr->forbegin;
+    if($forbegin == 1){
+        $icon_begin = '<span class="mini-tag easy-tag">입문</span>';
+    }else{
+        $icon_begin ='';
+    }
+    $forbasic = $lr->forbasic;
+    if($forbasic == 1){
+        $icon_basic = '<span class="mini-tag easy-tag">초급</span>';
+    }else{
+        $icon_basic ='';
+    }
+    $forinter = $lr->forinter;
+    if($forinter == 1){
+        $icon_inter = '<span class="mini-tag difficult-tag">중급</span>';
+    }else{
+        $icon_inter ='';
+    }
+    $foradv = $lr->foradv;
+    if($foradv == 1){
+        $icon_adv = '<span class="mini-tag difficult-tag">고급</span>';
+    }else{
+        $icon_adv ='';
+    }
+?>
+<?php echo $icon_new; ?><?php echo $icon_hit; ?><?php echo $icon_recom; ?><?php echo $icon_begin; ?><?php echo $icon_inter; ?><?php echo $icon_adv; ?><?php echo $icon_hit; ?>

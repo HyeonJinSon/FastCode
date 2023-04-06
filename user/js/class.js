a4160d44d1fa06c047f0b1a800b5e62eb65d2f15
CelineSunrise <119560572+CelineSunrise@users.noreply.github.com>
2023-04-06 11:36:56 +0900

    Update class.js

--- user/js/class.js
+++ user/js/class.js
@@ -1,16 +1,12 @@
+let span = $('.class_banner').find('div').find('span');
 
-let contH = $('#lec_list').outerHeight();
-sliding(contH);
-
-$('.lec_btn').find('a').filter(':first-of-type').click((e)=>{
-  e.preventDefault();
-  $('#lec_container').toggleClass('slide'); 
-  let tg = $('#lec_review').innerHeight() - 50;
-  sliding(tg+contH);
+span.each(function(){
+  let targetNum = $(this).attr('data-rate');
+  let num = 0;
+  let animation = setInterval(()=>{
+    $(this).text(++num);
+    if(num == targetNum){
+      clearInterval(animation);
+    }
+  },100);
 });
-
-
-function sliding(height){
-  $('#lec_container').css({height:`${height}px`});
-}
-

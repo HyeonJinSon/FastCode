b6f626982749f67664dfe9f8575ee1c9e4836f44
mi222ng <mi222ng@naver.com>
2023-04-06 14:35:25 +0900

    로그아웃 경로 수정

--- inc/user/header.php
+++ inc/user/header.php
@@ -61,7 +61,7 @@
                             <span class="tip">장바구니</span>
                         </li>
                         <li>
-                            <button type="submit" id="logout" onclick="location.replace('member/logout.php');"><i class="fa-solid fa-arrow-right-from-bracket"></i></button>
+                            <button type="submit" id="logout" onclick="location.replace('../member/logout.php');"><i class="fa-solid fa-arrow-right-from-bracket"></i></button>
                             <span class="tip">로그아웃</span>
                         </li>
                         <li>

d22fd3662e1136db250fa70b92f4d427a0d3a071
mi222ng <mi222ng@naver.com>
2023-04-06 16:47:28 +0900

    Update checkout_ok.php

--- user/cart/checkout_ok.php
+++ user/cart/checkout_ok.php
@@ -3,30 +3,33 @@
   include $_SERVER["DOCUMENT_ROOT"]."/inc/db.php";
   ini_set( 'display_errors', '0' );
 
-  $lecids = $_POST['lecid'];
-  $cartids = $_POST['cartid'];
-  $userid = $_POST['userid'];
-  $ucid = $_POST['ucid'];
+  $lecids = $_POST['lecid']; //강좌 번호
+  $cartids = $_POST['cartid']; //장바구니 번호
+  $userid = $_POST['userid']; //사용자 번호
+  $ucid = $_POST['ucid']; //사용자 쿠폰 번호
+  $date = date("Y-m-d:H-i-s");
 
   foreach($lecids as $lecid){
-    //user_lectures 테이블에서 결제완료된 lecid와 userid 의 값을 각 칼럼에 넣어준다.
-    $sql="INSERT INTO user_lectures (`lecid`,`userid`) VALUES ('".$lecid."','".$userid."')";
+    //user_lectures 테이블에 결제완료된 lecid와 userid 의 값을 각 칼럼에 넣어준다.
+    $sql="INSERT INTO user_lectures (`lecid`,`userid`,`reg_date`) VALUES ('".$lecid."','".$userid."','".$date."')";
     $result2 = $mysqli->query($sql) or die($mysqli->error);
     
     foreach($cartids as $cartid){
-    //완료되면 cart 테이블에서 결제되어 넘어간 강좌의 값을 없애준다.
+    //cart 테이블에 결제되어 넘어간 강좌의 값을 없애준다.
       $sql2 ="DELETE from cart where cartid='".$cartid."' and userid='".$userid."'" ;
       $mysqli->query($sql2);
     }
-    //완료되면 usercoupons 테이블에서 사용한 쿠폰의 값을 없애준다.
+    //user_coupons 테이블에 사용한 쿠폰의 값을 없애준다.
       $sql3 ="DELETE from user_coupons where ucid='".$ucid."' and userid='".$userid."'" ;
       $mysqli->query($sql3);
   }
+  
   if($result2){
     $data = array("result"=>"ok");
   }else{
     $data = array("result"=>"fail");
   }
+
   echo json_encode($data);
 
 

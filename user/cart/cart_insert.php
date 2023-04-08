658708403661003fb928203556f5362bcd512216
mi222ng <mi222ng@naver.com>
2023-04-08 15:40:37 +0900

    장바구니 기능 추가
    강좌보기에서 장바구니 눌렀을때 카트에 있는 강좌, 이미 수강중인 강좌 확인 후 알림

--- user/cart/cart_insert.php
+++ user/cart/cart_insert.php
@@ -3,24 +3,40 @@
     include $_SERVER["DOCUMENT_ROOT"]."/inc/db.php";
     ini_set( 'display_errors', '0' );
 
+
+
+    if(!isset($_SESSION['USERID'])){ //로그인 안되어있으면
+        echo '<script> alert("로그인을 해주세요.");
+        location.href="http://fastcode.dothome.co.kr/user/member/login.php";
+        </script>';
+        exit;
+    } 
+    
     $lecid = $_POST['lecid'];
     $userid = $_POST['userid'];
 
-    if(!$userid){//로그인 안되어있으면
-      echo '<script>
-      alert("로그인을 해주세요.");
-      location.href="http://fastcode.dothome.co.kr/user/member/login.php";
-      </script>';
-    }
-    $sql="INSERT INTO `cart`(`lecid`,`userid`,`cnt`,`regdate`)
-        VALUES('".$lecid."','".$userid."','1',now())";
-    $result=$mysqli->query($sql) or die($mysqli->error);
-    if($result){
-        $data = array("result"=>"ok","msg"=>"장바구니에 담겼습니다.");
+    //cart 테이블과 user_lectures에 동일한 lecid가 있는지 조회.
+    $query1 = "SELECT lecid from cart where userid='".$userid."'AND lecid='".$lecid."'";
+    $result1 = $mysqli->query($query1) or die("query error => ".$mysqli->error);
+    $rs1 = $result1->fetch_object();
+
+    $query2 = "SELECT lecid from user_lectures where userid='".$userid."' AND lecid='".$lecid."'";
+    $result2 = $mysqli->query($query2) or die("query error => ".$mysqli->error);
+    $rs2 = $result2->fetch_object();
+    
+    if($rs1){
+        $data = array("result"=>"fail","msg"=>"이미 장바구니에 담겼습니다.");
+    } else if($rs2){
+        $data = array("result"=>"fail","msg"=>"수강중인 강좌입니다.");
     }else{
-        $data = array("result"=>"fail","msg"=>"실패했습니다. 다시 시도해주세요.");
+        $sql = "INSERT INTO `cart`(`lecid`,`userid`,`cnt`,`regdate`) VALUES('".$lecid."','".$userid."','1',now())";
+        $result = $mysqli->query($sql) or die($mysqli->error);
+        if($result){
+            $data = array("result"=>"ok","msg"=>"장바구니에 담겼습니다.");
+        }
     }
     echo json_encode($data);
+    
 
 
 ?>

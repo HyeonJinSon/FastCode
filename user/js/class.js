ebca7517fb739c9b9ff3427cde610ddc94a1aa81
CelineSunrise <119560572+CelineSunrise@users.noreply.github.com>
2023-04-12 01:20:27 +0900

    Update class.js

--- user/js/class.js
+++ user/js/class.js
@@ -1,31 +1,25 @@
-let num2 = 0;
-$('.myLec').each(function(){
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth()+1;
-  let day = date.getDate();
-    if ((day+"").length < 2) {
-        day = "0" + day;
-    }
-    if ((month+"").length < 2) {
-      month = "0" + month;
-    }
-    let getToday = year+"-"+month+"-"+day; // 오늘 날짜 (2017-02-07)
-    console.log(getToday);
-    if($(this).attr('data-prg') == '100.00' || $(this).attr('data-date') < getToday){
-      $(this).addClass('lec_disabled');
-    }
 
-    if($(this).hasClass('lec_disabled')){
-      ++num2;
+let numCount = $('.class_banner .count');
+
+numCount.each(function(){
+  if(targetNum != 0){
+    let target = $(this).find('span');
+    let targetNum = target.attr('data-rate');
+    let num = 0;
+    let timer = setInterval(function(){
+      ++num;
+      target.text(num);
+      if(num == targetNum){
+        clearInterval(timer);
+      }
+    },120);
+  }
+});
+
+
+if($('.myLec').hasClass('lec_disabled')){
       $(this).attr('data-sort', 1);
-    }
-  });
-$('#bye').attr('data-rate',num2);
-
-let num1 = $('#ing').attr('data-tg') - num2;
-$('#ing').attr('data-rate',num1);
-
+}
 function liSort(tg, dataNm){
   tg.html(
     tg.find('li').sort(function(a,b){
@@ -35,14 +29,4 @@ function liSort(tg, dataNm){
 }
 liSort($('.myLec_container'), 'data-sort');
 
-let span = $('.class_banner').find('div').find('span');
-  span.each(function(){
-    let targetNum = $(this).attr('data-rate');
-    let num = 0;
-    let animation = setInterval(()=>{
-      $(this).text(++num);
-      if(num == targetNum){
-        clearInterval(animation);
-      }
-    },100);
-  });
+

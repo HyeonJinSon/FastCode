c7536b36cb47f56d788ac9ee48fd93b872f16ab9
HyeonJinSon <119560821+HyeonJinSon@users.noreply.github.com>
2023-04-12 15:02:05 +0900

    footer에 common.js 추가

--- inc/user/footer.php
+++ inc/user/footer.php
@@ -50,3 +50,4 @@
         <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
         <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+        <script src="../js/common.js"></script>

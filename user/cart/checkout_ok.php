769e6f07b0561e656ea6ff31e4bb3ce0fd7a9e36
HyeonJinSon <119560821+HyeonJinSon@users.noreply.github.com>
2023-04-13 11:51:38 +0900

    lectures 테이블 판매수 증가 쿼리 추가

--- user/cart/checkout_ok.php
+++ user/cart/checkout_ok.php
@@ -14,6 +14,15 @@
     $sql="INSERT INTO user_lectures (`lecid`,`userid`,`reg_date`) VALUES ('".$lecid."','".$userid."','".$date."')";
     $result2 = $mysqli->query($sql) or die($mysqli->error);
     
+    //lectures 테이블에 해당 강좌의 판매수를 1 올려준다
+    $sql4="SELECT * FROM lectures where lecid = '".$lecid."' ";
+    $result4 = $mysqli->query($sql4) or die($mysqli->error);
+    $rs4 = $result4 ->fetch_object();
+    $sale_cnt = $rs4 -> sale_cnt ;
+    $new_sale_cnt = $sale_cnt + 1;
+    $sql5 = "UPDATE lectures set sale_cnt = '{$new_sale_cnt}' where lecid = '".$lecid."' ";
+    $mysqli->query($sql5);    
+    
     foreach($cartids as $cartid){
     //cart 테이블에 결제되어 넘어간 강좌의 값을 없애준다.
       $sql2 ="DELETE from cart where cartid='".$cartid."' and userid='".$userid."'" ;
@@ -22,6 +31,7 @@
     //user_coupons 테이블에 사용한 쿠폰의 값을 없애준다.
       $sql3 ="DELETE from user_coupons where ucid='".$ucid."' and userid='".$userid."'" ;
       $mysqli->query($sql3);
+
   }
   
   if($result2){
